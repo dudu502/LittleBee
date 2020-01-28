2d7eb1f6042ac05cc9f9da193a4880653e39f069
panhj2 <panhj2@lenovo.com>
2020-01-28 19:37:49 +0800

    fix bug

--- Assets/Scripts/Src/Common/ByteBuffer.cs
+++ Assets/Scripts/Src/Common/ByteBuffer.cs
@@ -6,279 +6,139 @@ using System.Text;
 
 public class ByteBuffer : IDisposable
 {
-    //private byte[] bytes = new byte[0];
-    //private int position = 0;
-    //public ByteBuffer(byte[] value)
-    //{
-    //    source = value;
-    //}
-    //public ByteBuffer()
-    //{
-
-    //}
-
-    //public byte[] Getbuffer() { return source; }
-    //public void ResetPosition()
-    //{
-    //    position = 0;
-    //}
-    //public int GetPosition() { return position; }
-    //public byte[] source
-    //{
-    //    get { return bytes; }
-    //    set
-    //    {
-    //        bytes = value;
-    //        ResetPosition();
-    //    }
-    //}
-    //public ByteBuffer WriteInt32(int value)
-    //{
-    //    copy(BitConverter.GetBytes(value));
-    //    return this;
-    //}
-
-    //public int ReadInt32()
-    //{
-    //    return BitConverter.ToInt32(get(4), 0);
-    //}
-
-    //public ByteBuffer WriteShort(short value)
-    //{
-    //    copy(BitConverter.GetBytes(value));
-    //    return this;
-    //}
-    //public short ReadShort()
-    //{
-    //    return BitConverter.ToInt16(get(2), 0);
-    //}
-
-    //public ByteBuffer WriteString(string value)
-    //{
-    //    byte[] data = Encoding.UTF8.GetBytes(value);
-    //    WriteInt32(data.Length);
-    //    copy(data);
-    //    return this;
-    //}
-    //public string ReadString()
-    //{
-    //    return Encoding.UTF8.GetString(get(ReadInt32()));
-    //}
-
-    //public ByteBuffer WriteBytes(byte[] value)
-    //{
-    //    WriteInt32(value.Length);
-    //    copy(value);
-    //    return this;
-    //}
-    //public byte[] ReadBytes()
-    //{
-    //    return get(ReadInt32());
-    //}
-
-
-    //public ByteBuffer WriteByte(byte value)
-    //{
-    //    copy(new byte[] { value });
-    //    return this;
-    //}
-    //public byte ReadByte()
-    //{
-    //    return get(1)[0];
-    //}
-    //public ByteBuffer WriteBool(bool value)
-    //{
-    //    WriteByte(Convert.ToByte(value));
-    //    return this;
-    //}
-    //public bool ReadBool()
-    //{
-    //    return Convert.ToBoolean(ReadByte());
-    //}
-    //public ByteBuffer WriteLong(long value)
-    //{
-    //    copy(BitConverter.GetBytes(value));
-    //    return this;
-    //}
-    //public long ReadLong()
-    //{
-    //    return BitConverter.ToInt32(get(8), 0);
-    //}
-
-    //public ByteBuffer WriteFloat(float value)
-    //{
-    //    copy(BitConverter.GetBytes(value));
-    //    return this;
-    //}
-    //public float ReadFloat()
-    //{
-    //    return BitConverter.ToSingle(get(4), 0);
-    //}
-
-    //private void copy(byte[] value)
-    //{
-    //    byte[] temps = new byte[bytes.Length + value.Length];
-    //    Buffer.BlockCopy(bytes, 0, temps, 0, bytes.Length);
-    //    Buffer.BlockCopy(value, 0, temps, position, value.Length);
-
-
-    //    position += value.Length;
-    //    bytes = temps;
-    //    temps = null;
-    //}
-    //private byte[] get(int length)
-    //{
-    //    byte[] data = new byte[length];
-    //    Buffer.BlockCopy(bytes, position, data, 0, length);
-    //    position += length;
-    //    return data;
-    //}
-    //public void Dispose()
-    //{
-    //    position = 0;
-    //    bytes = null;
-    //}
-
-    BinaryWriter bw;
-    BinaryReader br;
-    MemoryStream ms = new MemoryStream();
-    BinaryWriter Writer
+    private byte[] bytes = new byte[0];
+    private int position = 0;
+    public ByteBuffer(byte[] value)
     {
-        get
-        {
-            if (bw == null) bw = new BinaryWriter(ms);
-            return bw;
-        }
-    }
-    BinaryReader Reader
-    {
-        get
-        {
-            if (br == null) br = new BinaryReader(ms);
-            return br;
-        }
-    }
-
-    public ByteBuffer(byte[] bytes)
-    {
-        Writer.Write(bytes);
-        ms.Position = 0;
+        source = value;
     }
     public ByteBuffer()
     {
 
     }
 
-    public byte[] Getbuffer()
+    public byte[] Getbuffer() { return source; }
+    public void ResetPosition()
     {
-        return ms.GetBuffer();
+        position = 0;
+    }
+    public int GetPosition() { return position; }
+    public byte[] source
+    {
+        get { return bytes; }
+        set
+        {
+            bytes = value;
+            ResetPosition();
+        }
     }
     public ByteBuffer WriteInt32(int value)
     {
-        Writer.Write(value);
+        copy(BitConverter.GetBytes(value));
         return this;
     }
-    public ByteBuffer WriteByte(byte value)
-    {
-        Writer.Write(value);
-        return this;
-    }
-    public byte ReadByte()
-    {
-        return Reader.ReadByte();
-    }
-    public int GetPosition()
-    {
-        return (int)ms.Position;
-    }
-    public ByteBuffer WriteBool(bool value)
-    {
-        Writer.Write(value);
-        return this;
-    }
-    public bool ReadBool()
-    {
-        return Reader.ReadBoolean();
-    }
+
     public int ReadInt32()
     {
-        return Reader.ReadInt32();
+        return BitConverter.ToInt32(get(4), 0);
     }
-    public ByteBuffer WriteLong(long value)
-    {
-        Writer.Write(value);
-        return this;
-    }
-    public long ReadLong()
-    {
-        return Reader.ReadInt64();
-    }
-    public ByteBuffer WriteFloat(float value)
-    {
-        Writer.Write(value);
-        return this;
-    }
-    public float ReadFloat()
-    {
-        return Reader.ReadSingle();
-    }
+
     public ByteBuffer WriteShort(short value)
     {
-        Writer.Write(value);
+        copy(BitConverter.GetBytes(value));
         return this;
     }
     public short ReadShort()
     {
-        return Reader.ReadInt16();
+        return BitConverter.ToInt16(get(2), 0);
     }
 
     public ByteBuffer WriteString(string value)
     {
-        byte[] bytes = Encoding.UTF8.GetBytes(value);
-        Writer.Write((int)bytes.Length);
-        Writer.Write(bytes);
-
+        byte[] data = Encoding.UTF8.GetBytes(value);
+        WriteInt32(data.Length);
+        copy(data);
         return this;
     }
     public string ReadString()
     {
-        int len = ReadInt32();
-        byte[] buffer = new byte[len];
-        buffer = Reader.ReadBytes(len);
-        return Encoding.UTF8.GetString(buffer);
+        return Encoding.UTF8.GetString(get(ReadInt32()));
     }
 
     public ByteBuffer WriteBytes(byte[] value)
     {
         WriteInt32(value.Length);
-        Writer.Write(value);
+        copy(value);
         return this;
     }
     public byte[] ReadBytes()
     {
-        int len = ReadInt32();
-        return Reader.ReadBytes(len);
+        return get(ReadInt32());
     }
 
 
+    public ByteBuffer WriteByte(byte value)
+    {
+        copy(new byte[] { value });
+        return this;
+    }
+    public byte ReadByte()
+    {
+        return get(1)[0];
+    }
+    public ByteBuffer WriteBool(bool value)
+    {
+        WriteByte(Convert.ToByte(value));
+        return this;
+    }
+    public bool ReadBool()
+    {
+        return Convert.ToBoolean(ReadByte());
+    }
+    public ByteBuffer WriteLong(long value)
+    {
+        copy(BitConverter.GetBytes(value));
+        return this;
+    }
+    public long ReadLong()
+    {
+        return BitConverter.ToInt32(get(8), 0);
+    }
+
+    public ByteBuffer WriteFloat(float value)
+    {
+        copy(BitConverter.GetBytes(value));
+        return this;
+    }
+    public float ReadFloat()
+    {
+        return BitConverter.ToSingle(get(4), 0);
+    }
+
+    private void copy(byte[] value)
+    {
+        byte[] temps = new byte[bytes.Length + value.Length];
+        Buffer.BlockCopy(bytes, 0, temps, 0, bytes.Length);
+        Buffer.BlockCopy(value, 0, temps, position, value.Length);
+
+
+        position += value.Length;
+        bytes = temps;
+        temps = null;
+    }
+    private byte[] get(int length)
+    {
+        byte[] data = new byte[length];
+        Buffer.BlockCopy(bytes, position, data, 0, length);
+        position += length;
+        return data;
+    }
     public void Dispose()
     {
-        ms.Close();
-        ms.Dispose();
-        ms = null;
-        if (bw != null)
-        {
-            bw.Close();
-            bw.Dispose();
-            bw = null;
-        }
-        if (br != null)
-        {
-            br.Close();
-            br.Dispose();
-            bw = null;
-        }
+        position = 0;
+        bytes = null;
     }
+
 
 
     //压缩字节
